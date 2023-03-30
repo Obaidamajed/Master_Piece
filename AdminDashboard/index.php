c1c0a8d4c720152cf8b3b02e39f10eb9d8f29acc
obaida <obaidaalthunibat@gmail.com>
2023-03-31 01:32:40 +0300

    Css become under comment in index.php to prevent appear error

--- AdminDashboard/index.php
+++ AdminDashboard/index.php
@@ -1,25 +1,4 @@
-<?php include('Includes/db.php'); ?>
-<!doctype html>
-<html lang="en">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <style>
-            <?php
-            $css = file_get_contents('../css/style.css');
-            echo $css;
-            ?>
-        </style>
-        <!-- Fontawesome  -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" >
-
-        <title>Dashboard FA.</title>
-    </head>
-    <body>
+<?php  include('../Includes/header.php'); ?>
 
 <!--Start ** This Code to prevent users entering into AdminDashboard -->
     <?php if(!isset($_GET['id']) or !is_numeric($_GET['id'])) {
